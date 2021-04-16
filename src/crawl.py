4a9ee9724fb73055651f10ea2a24f64d8b4fb0d1
Kirtana <ks3057@rit.edu>
2021-04-16 18:00:56 -0400

    Polishing up final csv

--- src/crawl.py
+++ src/crawl.py
@@ -1,24 +1,26 @@
 from pydriller import RepositoryMining
 import time
-import csv
 from datetime import datetime
 
 
 def initialise():
-    # commons_pool()
-    # jackson_core()
-    # commons_dbcp()
-    # commons_fileupload()
-    # commons_compress()
+    f = open("outputs/final.csv", "w")
+    f.write("commit hash, author name, filename, nloc, cyclomatic complexity, "
+            "num lines added, "
+            "num lines removed, label" + "\n")
+
+    commons_pool()
+    jackson_core()
+    commons_dbcp()
+    commons_fileupload()
+    commons_compress()
     commons_io()
 
 
 def commons_pool():
     dt1 = datetime(2007, 12, 10, 00, 50, 7)
     dt2 = datetime(2014, 9, 23, 13, 7, 52)
-    count = 0
-    commit_classify = {}
-    commons_pool_reg = {'16a2ef563c9d87253a2347628f796776d0987851',
+    performance_regression_commits_from_peass = {'16a2ef563c9d87253a2347628f796776d0987851',
                         '094266f6a51a974ffb6dc264bcb6f7c13801fd96',
                         '2113166ed6a359adfcde240310960f1aee3f2607',
                         '1d9e39095ccecdf3668236177bc17b4561382316',
@@ -27,30 +29,20 @@ def commons_pool():
                         '5392bccc645803a66541ec1e01172a22f50da7d9',
                         '6df9afdb2c5125beb388b67570f73818d660aa12'}
 
-    for commit in RepositoryMining(
-            '/Users/kirtanasuresh/Documents/commons-pool',
-            only_in_branch="master", only_no_merge=True, since=dt1,
-            to=dt2).traverse_commits():
-        if commit.hash in commons_pool_reg:
-            commit_classify[commit.hash] = "PR"
-            count += 1
-        else:
-            commit_classify[commit.hash] = "NPR"
+    number_of_non_perf_files_required = len(
+        performance_regression_commits_from_peass)
+    number_of_non_perf_files_current = 0
 
-    with open('outputs/commons-pool.csv', 'w') as f:
-        w = csv.writer(f, delimiter=',')
-        w.writerow(["hashcode", "performance regression"])
-        for key, values in commit_classify.items():
-            w.writerow([key, values])
+    build_csv(dt1, dt2, performance_regression_commits_from_peass,
+              "commons-pool",
+              number_of_non_perf_files_required,
+              number_of_non_perf_files_current)
 
 
 def jackson_core():
     dt1 = datetime(2013, 10, 3, 10, 8, 27)
     dt2 = datetime(2019, 2, 16, 00, 23, 00)
-    count = 0
-    commit_classify = {}
-    commit_identified = set()
-    jackson_core_reg = {
+    performance_regression_commits_from_peass = {
         '8fc4243d0f15813c9caf37635113489854cbd655',
         'ac956fe5eed337c516beb504aadf82917c26d6ba',
         'ef860da507ef31a604624a434040bde984d310e8',
@@ -63,103 +55,52 @@ def jackson_core():
         'b6c54cf06167b7ef8b45a67e8ddfa427e63c6c20',
         '8cd1e24ad7cb031b14d90db64c49ba0a68669463'}
 
-    for commit in RepositoryMining(
-            '/Users/kirtanasuresh/Documents/jackson-core',
-            only_in_branch="master", only_no_merge=True, since=dt1,
-            to=dt2).traverse_commits():
-        if commit.hash in jackson_core_reg:
-            commit_classify[commit.hash] = "PR"
-            count += 1
-            commit_identified.add(commit.hash)
-        else:
-            commit_classify[commit.hash] = "NPR"
+    number_of_non_perf_files_required = len(
+        performance_regression_commits_from_peass)
+    number_of_non_perf_files_current = 0
 
-    with open('outputs/jackson-core.csv', 'w') as f:
-        w = csv.writer(f, delimiter=',')
-        w.writerow(["hashcode", "performance regression"])
-        for key, values in commit_classify.items():
-            w.writerow([key, values])
-
-    print(count)
-    f = open("outputs/jackson-core-ignored.txt", "w")
-    f.write(str(jackson_core_reg.difference(commit_identified)))
-    f.close()
+    build_csv(dt1, dt2, performance_regression_commits_from_peass,
+              "jackson-core",
+              number_of_non_perf_files_required,
+              number_of_non_perf_files_current)
 
 
 def commons_dbcp():
     dt1 = datetime(2014, 2, 5, 22, 30, 34)
     dt2 = datetime(2014, 3, 3, 13, 43, 30)
-    count = 0
-    commit_classify = {}
-    commit_identified = set()
-    commons_dbcp_reg = {
+    performance_regression_commits_from_peass = {
         'a3cefc2063d220efdc8a42a66164a5ff0bfd690a',
         'a127eaf6b81f3be06282e4b67692bacc020b8703'}
 
-    for commit in RepositoryMining(
-            '/Users/kirtanasuresh/Documents/commons-dbcp',
-            only_in_branch="master", only_no_merge=True, since=dt1,
-            to=dt2).traverse_commits():
-        if commit.hash in commons_dbcp_reg:
-            commit_classify[commit.hash] = "PR"
-            count += 1
-            commit_identified.add(commit.hash)
-        else:
-            commit_classify[commit.hash] = "NPR"
+    number_of_non_perf_files_required = len(performance_regression_commits_from_peass)
+    number_of_non_perf_files_current = 0
 
-    with open('outputs/commons-dbcp.csv', 'w') as f:
-        w = csv.writer(f, delimiter=',')
-        w.writerow(["hashcode", "performance regression"])
-        for key, values in commit_classify.items():
-            w.writerow([key, values])
-
-    print(count)
-    f = open("outputs/commons-dbcp-ignored.txt", "w")
-    f.write(str(commons_dbcp_reg.difference(commit_identified)))
-    f.close()
+    build_csv(dt1, dt2, performance_regression_commits_from_peass,
+              "commons-dbcp",
+              number_of_non_perf_files_required,
+              number_of_non_perf_files_current)
 
 
 def commons_fileupload():
     dt1 = datetime(2013, 3, 7, 10, 58, 30)
     dt2 = datetime(2016, 8, 22, 15, 54, 00)
-    count = 0
-    commit_classify = {}
-    commit_identified = set()
-    commons_fileupload_reg = {
+    performance_regression_commits_from_peass = {
         '651283e071de6230356af058afe0c0b78e8cb8b6',
         '774ef160d591b579f703c694002e080f99bcd28b',
         'afdedc9580dde03287979607f9ce441b23b6bc90'}
 
-    for commit in RepositoryMining(
-            '/Users/kirtanasuresh/Documents/commons-fileupload',
-            only_in_branch="master", only_no_merge=True, since=dt1,
-            to=dt2).traverse_commits():
-        if commit.hash in commons_fileupload_reg:
-            commit_classify[commit.hash] = "PR"
-            count += 1
-            commit_identified.add(commit.hash)
-        else:
-            commit_classify[commit.hash] = "NPR"
+    number_of_non_perf_files_required = len(performance_regression_commits_from_peass)
+    number_of_non_perf_files_current = 0
 
-    with open('outputs/commons-fileupload.csv', 'w') as f:
-        w = csv.writer(f, delimiter=',')
-        w.writerow(["hashcode", "performance regression"])
-        for key, values in commit_classify.items():
-            w.writerow([key, values])
-
-    print(count)
-    f = open("outputs/commons-fileupload-ignored.txt", "w")
-    f.write(str(commons_fileupload_reg.difference(commit_identified)))
-    f.close()
+    build_csv(dt1, dt2, performance_regression_commits_from_peass, "commons-fileupload",
+              number_of_non_perf_files_required,
+              number_of_non_perf_files_current)
 
 
 def commons_compress():
     dt1 = datetime(2009, 9, 30, 7, 25, 00)
     dt2 = datetime(2018, 1, 10, 10, 10, 00)
-    count = 0
-    commit_classify = {}
-    commit_identified = set()
-    commons_fileupload_reg = {
+    performance_regression_commits_from_peass = {
         '2318ba308b4d36c493330c9286a86878bc24af55',
         '21148e7b5bc4cf6d0c38dc80bed5d7eb709897e1',
         'a2f978e97f13697e7adfddc3f96a9131803db679',
@@ -176,51 +117,41 @@ def commons_compress():
         '1418705648202405208e3f4d900dfcc81457cbac',
         '516c38fd95fff30841f9af370f50cf1f7733c209'}
 
-    for commit in RepositoryMining(
-            '/Users/kirtanasuresh/Documents/commons-compress',
-            only_in_branch="master", only_no_merge=True, since=dt1,
-            to=dt2).traverse_commits():
-        if commit.hash in commons_fileupload_reg:
-            commit_classify[commit.hash] = "PR"
-            count += 1
-            commit_identified.add(commit.hash)
-        else:
-            commit_classify[commit.hash] = "NPR"
+    number_of_non_perf_files_required = len(performance_regression_commits_from_peass)
+    number_of_non_perf_files_current = 0
 
-    with open('outputs/commons-compress.csv', 'w') as f:
-        w = csv.writer(f, delimiter=',')
-        w.writerow(["hashcode", "performance regression"])
-        for key, values in commit_classify.items():
-            w.writerow([key, values])
-
-    print(count)
-    f = open("outputs/commons-compress-ignored.txt", "w")
-    f.write(str(commons_fileupload_reg.difference(commit_identified)))
-    f.close()
+    build_csv(dt1, dt2, performance_regression_commits_from_peass, "commons-compress",
+              number_of_non_perf_files_required,
+              number_of_non_perf_files_current)
 
 
 def commons_io():
     dt1 = datetime(2006, 12, 29, 13, 45, 00)
     dt2 = datetime(2012, 6, 1, 17, 27, 22)
-    commit_classify = {}
-    commit_identified = set()
-    commons_fileupload_reg = {
+    performance_regression_commits_from_peass = {
         '6eca4c05986bac88d1b65af83348a754efa36559',
         '59ffcad15d220c2bc1f70f01d58bc31dec04b423'}
 
-    number_of_non_perf_files_required = len(commons_fileupload_reg)
+    number_of_non_perf_files_required = len(performance_regression_commits_from_peass)
     number_of_non_perf_files_current = 0
 
-    f = open("outputs/commons-io-expanded.csv", "w")
-    f.write("commit hash, author name, filename, nloc, cyclomatic complexity, "
-            "num lines added, "
-            "num lines removed, label")
+    build_csv(dt1, dt2, performance_regression_commits_from_peass, "commons-io",
+              number_of_non_perf_files_required,
+              number_of_non_perf_files_current)
+
+
+def build_csv(dt1, dt2, performance_regression_commits_from_peass, project,
+              number_of_non_perf_files_required,
+              number_of_non_perf_files_current):
+
+    commit_classify = {}
+    commit_identified = set()
 
     for commit in RepositoryMining(
-            '/Users/kirtanasuresh/Documents/commons-io',
+            '/Users/kirtanasuresh/Documents/{}'.format(project),
             only_in_branch="master", only_no_merge=True, since=dt1,
             to=dt2).traverse_commits():
-        if commit.hash in commons_fileupload_reg:
+        if commit.hash in performance_regression_commits_from_peass:
             commit_classify[commit.hash] = "PR"
             commit_identified.add(commit.hash)
         else:
@@ -231,19 +162,29 @@ def commons_io():
                 commit_classify[commit.hash] = "NPR"
                 number_of_non_perf_files_current += 1
 
-        for m in commit.modifications:
-            f = open("outputs/commons-io-expanded.csv", "a")
-            list1 = [commit.hash, commit.author.name, m.filename,
-                     m.nloc, m.complexity,
-                     m.added, m.removed,
-                     commit_classify[commit.hash]]
-            list1 = map(str, list1)
-            x = ",".join(list1)
-            x = "\n" + x
-            f.write(x)
+        # if the change is in the test case itself, we ignore the commit
+        commit_has_changed_tests = False
 
-        f = open("outputs/commons-compress-ignored.txt", "w")
-        f.write(str(commons_fileupload_reg.difference(commit_identified)))
+        # here we check only for NPR label commits because we excluded
+        # Test commits when manually evaluating PR label
+        if commit_classify[commit.hash] == "NPR":
+            for m in commit.modifications:
+                if "Test" in m.filename or "test" in m.filename:
+                    commit_has_changed_tests = commit_has_changed_tests or True
+        if not commit_has_changed_tests:
+            for m in commit.modifications:
+                f = open("outputs/final.csv", "a")
+                list1 = [commit.hash, commit.author.name, m.filename,
+                         m.nloc, m.complexity,
+                         m.added, m.removed,
+                         commit_classify[commit.hash]]
+                list1 = map(str, list1)
+                x = ",".join(list1)
+                x = "\n" + x
+                f.write(x)
+
+        f = open("outputs/{}-ignored.txt".format(project), "w")
+        f.write(str(performance_regression_commits_from_peass.difference(commit_identified)))
         f.close()
 
 
